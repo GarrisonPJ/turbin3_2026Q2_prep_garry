19fd19bf3c160a88b09c780698d6cc215cb8d845
Garry <GarryPeterJr@outlook.com>
2026-02-27 16:13:19 +0800

    day1 keygen.ts

---
+++ Record.md
@@ -0,0 +1,2 @@
+2026/2/27
+rewrited /pre-builder/typescript/keygen.ts. Removed 2functions of Base58 code conversion. left one function for checking existed wallet or generating a new one.
--- pre-builders/typescript/keygen.ts
+++ pre-builders/typescript/keygen.ts
@@ -1,41 +1,30 @@
 import { Keypair } from '@solana/web3.js';
-import bs58 from 'bs58';
-import promptSync from 'prompt-sync';
-const prompt = promptSync();
+import { existsSync, readFileSync, writeFileSync } from 'fs';
 
-//generate keypair
-let kp = Keypair.generate();
+function loadOrGenWallet() {
+  const WALLET = 'dev-wallet.json';
 
+  if (existsSync(WALLET)) {
+    const wallet = readFileSync(WALLET, 'utf-8');
 
-// log the keypair
-console.log(`You've generated a new Solana wallet: ${kp.publicKey.toBase58()}`);
+    console.log('you have a wallet already:');
+    console.log('wallet', wallet);
+  }
+  else {
+    console.log('your Keypair is generating');
 
-// log the secret key
-console.log(`[${kp.secretKey}]`);
+    //generate keypair
+    let keyPair = Keypair.generate();
 
+    //log the keypair & the secret key
+    console.log(`your new wallet is : ${keyPair.publicKey.toBase58()}`);
+    console.log(`[${keyPair.secretKey}]`);
 
+    // save the wallet
+    writeFileSync(WALLET, JSON.stringify(Array.from(keyPair.secretKey)));
+    console.log('your wallet is save to', WALLET);
 
-// Base58 to wallet bytes conversion
-function base58ToWallet(): void { // snake case, unlike rust's
-    const base58String = prompt('Enter base58 string: ');
-    try {
-        const walletBytes = bs58.decode(base58String);
-        console.log('Wallet bytes:', walletBytes);
-    } catch (error) {
-        console.error('Error decoding base58 string:', error);
-    }
+  }
 }
 
-// Wallet bytes to base58 conversion
-function walletToBase58(): void {
-    const walletBytes = kp.secretKey; // intead of passing the array
-    try {
-        const base58String = bs58.encode(walletBytes);
-        console.log('Base58 string:', base58String);
-    } catch (error) {
-        console.error('Error encoding wallet bytes:', error);
-    }
-}
-
-
-// wallet address: F9xaz55EjKA3UnaPaaqNyDM4WsNoHWRYi9hBAeNEQMWy
+loadOrGenWallet();
