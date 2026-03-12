77e3a3ff21f414c6d830500da83a0130baac278d
Garry <GarryPeterJr@outlook.com>
2026-03-12 23:07:45 +0800

    Day12 finished spl_transfer

--- core-starter/cluster1/spl_transfer.ts
+++ core-starter/cluster1/spl_transfer.ts
@@ -1,5 +1,12 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../turbin3-wallet.json"
+import "dotenv/config";
+import {
+  Commitment,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from "@solana/web3.js";
+import wallet from "../turbin3-wallet.json";
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
@@ -7,22 +14,49 @@ const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
+const connection = new Connection(
+  process.env.SOLANA_RPC_URL || "https://api.devnet.solana.com",
+  commitment,
+);
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+if (!process.env.WBA_MINT_ADDRESS)
+  throw new Error("[ERROR] WBA_MINT_ADDRESS is not in .env file");
+const mint = new PublicKey(process.env.WBA_MINT_ADDRESS);
 
 // Recipient address
-const to = new PublicKey("<receiver address>");
+const to = new PublicKey("HaZRyLZzRknszTHqHn4Kj1iu9uGr6Wiss3jPRDiVPQqz"); //Just used another wallet  of mine as a constant for convinence
 
 (async () => {
-    try {
-        // Get the token account of the fromWallet address, and if it does not exist, create it
+  try {
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+    const fromATA = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      keypair.publicKey,
+    );
+    // Get the token account of the toWallet address, and if it does not exist, create it
+    const toATA = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair,
+      mint,
+      to,
+    );
 
-        // Get the token account of the toWallet address, and if it does not exist, create it
+    // Transfer the new token to the "toTokenAccount" we just created
+    const signature = await transfer(
+      connection,
+      keypair,
+      fromATA.address,
+      toATA.address,
+      keypair,
+      100_000n,
+    );
 
-        // Transfer the new token to the "toTokenAccount" we just created
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+    console.log("[INFO] Success! Check your transaction:");
+    console.log(`https://explorer.solana.com/tx/${signature}?cluster=devnet`);
+  } catch (e) {
+    console.error(`[ERROR] Transfer went wrong and aborted: ${e}`);
+  }
 })();
